880fa81278f0f01a602137586a4b131aba010346
habib khan <hk7512809@gmail.com>
2026-03-10 00:29:38 -0700

    Add vercel.json for React Router fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <h2 className="logo">MySite</h2>
 
       <ul className="nav-links">
-        <li><Link to="/Home">Home</Link></li>
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/About">About</Link></li>
         <li><Link to="/Contact">Contact</Link></li>
       </ul>
